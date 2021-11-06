c406f0b4bbe629aacc6b59f48bf9550c95ee839a
Sung Paik <sungypaik@gmail.com>
2021-11-05 23:19:04 -0400

    included prettier file for the repo

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  useTabs: false,
+  jsxSingleQuote: false,
+  jsxBracket: false,
+  arrowParens: 'always',
+};
