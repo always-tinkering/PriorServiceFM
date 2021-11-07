5e1002748f1e64e457c3058f1b47c79c22b2c447
Sung Paik <sungypaik@gmail.com>
2021-11-06 21:31:16 -0400

    used header component on the home page and changed font for the website

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,22 +1,12 @@
 import React from "react";
 import InfoCard from "./InfoCard";
 import Footer from "./Footer";
+import Header from "./Header";
 
 export default function Home() {
   return (
     <div className="home">
-      <div className="header">
-        <div>
-          <div>
-            <img src="/images/iwojima.png" alt="Iwo Jima" />
-          </div>
-        </div>
-        <div>
-          <div>Opportunity</div>
-          <div>Explore</div>
-          <div>Apply</div>
-        </div>
-      </div>
+      <Header />
       <div
         style={{
           display: "flex",
